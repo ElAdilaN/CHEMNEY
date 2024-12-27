481da86203b563d7b9f2f0e401ee03ec03882c6a
Noureddin El Adila <noureddineladila@gmail.com>
2024-12-27 09:28:36 +0100

    day 1

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "custom-bg": "url('/bg.webp')", // path to the image in the public folder
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        ml: "940px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        col1: "#1A1A1A",
+        col2: "#D9A16F",
+        col3: "#1E1E1E",
+      },
+    },
+  },
+  plugins: [],
+};
